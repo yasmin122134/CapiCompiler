2a82197b42f2da762695ab6775a813b35d757c63
yasmin122134 <yasmin122134@gmail.com>
2024-11-29 16:08:26 +0200

    added interface for the user data access layer

---
+++ IUserDAL.h
@@ -0,0 +1,19 @@
+#ifndef IUserDAL_H
+#define IUserDAL_H
+
+#include "User.h"
+
+class IUserDAL
+{
+private:
+    /* data */
+public:
+    IUserDAL(/* args */);
+    virtual User getUser(int id); // returns user from file and creats new if doesn't exists
+    virtual void addUser(User user);
+    virtual void removeUser(User user); // remove a specific user from the file
+    virtual bool doesExist(User user);
+    virtual void clear();
+};
+
+#endif IUserDAL_H
