08cf68c024c441e02290ab03cc209d2ab8ce77c6
Omer <onavot06@gmail.com>
2024-12-05 20:49:52 +0200

    push

---
+++ build/CMakeFiles/MainApp.dir/DependInfo.cmake
@@ -0,0 +1,33 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/omer/CapiCompiler/App.cpp" "CMakeFiles/MainApp.dir/App.cpp.o" "gcc" "CMakeFiles/MainApp.dir/App.cpp.o.d"
+  "/Users/omer/CapiCompiler/ConsoleMenu.cpp" "CMakeFiles/MainApp.dir/ConsoleMenu.cpp.o" "gcc" "CMakeFiles/MainApp.dir/ConsoleMenu.cpp.o.d"
+  "/Users/omer/CapiCompiler/DataAccessLayer.cpp" "CMakeFiles/MainApp.dir/DataAccessLayer.cpp.o" "gcc" "CMakeFiles/MainApp.dir/DataAccessLayer.cpp.o.d"
+  "/Users/omer/CapiCompiler/IMenu.cpp" "CMakeFiles/MainApp.dir/IMenu.cpp.o" "gcc" "CMakeFiles/MainApp.dir/IMenu.cpp.o.d"
+  "/Users/omer/CapiCompiler/Main.cpp" "CMakeFiles/MainApp.dir/Main.cpp.o" "gcc" "CMakeFiles/MainApp.dir/Main.cpp.o.d"
+  "/Users/omer/CapiCompiler/Movie.cpp" "CMakeFiles/MainApp.dir/Movie.cpp.o" "gcc" "CMakeFiles/MainApp.dir/Movie.cpp.o.d"
+  "/Users/omer/CapiCompiler/User.cpp" "CMakeFiles/MainApp.dir/User.cpp.o" "gcc" "CMakeFiles/MainApp.dir/User.cpp.o.d"
+  "/Users/omer/CapiCompiler/commands/Add.cpp" "CMakeFiles/MainApp.dir/commands/Add.cpp.o" "gcc" "CMakeFiles/MainApp.dir/commands/Add.cpp.o.d"
+  "/Users/omer/CapiCompiler/commands/Help.cpp" "CMakeFiles/MainApp.dir/commands/Help.cpp.o" "gcc" "CMakeFiles/MainApp.dir/commands/Help.cpp.o.d"
+  "/Users/omer/CapiCompiler/commands/ICommand.cpp" "CMakeFiles/MainApp.dir/commands/ICommand.cpp.o" "gcc" "CMakeFiles/MainApp.dir/commands/ICommand.cpp.o.d"
+  "/Users/omer/CapiCompiler/commands/Recommend.cpp" "CMakeFiles/MainApp.dir/commands/Recommend.cpp.o" "gcc" "CMakeFiles/MainApp.dir/commands/Recommend.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
